c0ce2b2fa8064d2cbf1af452c82bf1fc96d0155e
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-03-02 17:59:32 +0900

    refactor : file directory settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+		<Html lang='ko'>
+			<Head>
+				<title>YUMU</title>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
