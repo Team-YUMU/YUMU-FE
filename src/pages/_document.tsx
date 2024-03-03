b68eaf4749d33641724e58374d2b8fdc98a9ad88
박지원 <jackgg12322@gmail.com>
2024-03-03 17:12:42 +0900

    test : esLint rule 적용 후 push 가능 테스트

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
+	return (
 		<Html lang='ko'>
 			<Head>
 				<title>YUMU</title>
