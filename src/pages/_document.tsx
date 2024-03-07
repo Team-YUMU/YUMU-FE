746fdc0350919c369e7d7d5467f5f161baa7828e
박지원 <jackgg12322@gmail.com>
2024-03-08 00:57:11 +0900

    refactor : Head title 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang='ko'>
-			<Head>
-				<title>YUMU</title>
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
