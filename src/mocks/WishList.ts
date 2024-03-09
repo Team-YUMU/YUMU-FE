b5a92a7d2bceaeb68b6b1fc24018feb541150a8a
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-03-09 11:18:39 +0900

    feat : 임시 Mockup Data 추가 (Auction, Member, PurchaseHistory, SalesHistory, WishList)

---
+++ src/mocks/WishList.ts
@@ -0,0 +1,16 @@
+import { WishList } from '@/types/types';
+
+const WishLists: WishList[] = [
+	{
+		id: 1,
+		memberId: 2,
+		auctionId: 4,
+	},
+	{
+		id: 1,
+		memberId: 2,
+		auctionId: 5,
+	},
+];
+
+export default WishLists;
