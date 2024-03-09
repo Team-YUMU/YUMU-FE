584af8f6dfda0755b3a03d7b5e4605f33db05557
Park Ji Won <124119421+PJW980921@users.noreply.github.com>
2024-03-09 12:54:10 +0900

    Mockup 데이터 구축 (#5) (#6)
    * Add gitignore
    
    * Update issue templates
    
    * test page 구현 테스트
    
    * test: 배포테스트
    
    * test: 배포 테스트 재도전
    
    * 메인 index 컴포넌트 수정, 배포재도전
    
    * fix : eslint rule 수정
    
    * chore : test
    
    * feat : types.ts에 사용자, 구매내역, 판매 내역, 관심 상품, 경매 게시물 추가
    
    * feat : 임시 Mockup Data 추가 (Auction, Member, PurchaseHistory, SalesHistory, WishList)
    
    * refactor : 타입명 변경 (Member -> MemberType)
    
    ---------
    
    Co-authored-by: TaciTa <79896328+00TaciTa00@users.noreply.github.com>
    Co-authored-by: Sungeunji <danceunji@gmail.com>

---
+++ src/mocks/WishList.ts
@@ -0,0 +1,16 @@
+import { WishListType } from '@/types/types';
+
+const WishLists: WishListType[] = [
+  {
+    id: 1,
+    memberId: 2,
+    auctionId: 4,
+  },
+  {
+    id: 1,
+    memberId: 2,
+    auctionId: 5,
+  },
+];
+
+export default WishLists;
---
+++ src/types/types.ts
@@ -0,0 +1,52 @@
+export type MemberType = {
+  id: number;
+  email: string;
+  nickname: string;
+  password: string;
+  account: null | string;
+  profileImage: null | string;
+  introduce: null | string;
+  snsLink: null | string;
+  address: null | string;
+};
+
+export type PurchaseHistoryType = {
+  id: number;
+  artTitle: string;
+  artist: string;
+  price: number;
+  artImage: string;
+  purchaseDate: Date;
+};
+
+export type SalesHistoryType = {
+  id: number;
+  artTitle: string;
+  artist: string;
+  price: number;
+  artImage: string;
+  status: string;
+  saleDate: Date;
+};
+
+export type WishListType = {
+  id: number;
+  memberId: number;
+  auctionId: number;
+};
+
+export type AuctionType = {
+  id: number;
+  artTitle: string;
+  artImage: string;
+  artDetail: string;
+  artist: string;
+  artSize: string;
+  artDate: Date;
+  startDate: Date;
+  startPrice: number;
+  endPrice: null | number;
+  bidder: null | string;
+  createdAt: Date;
+  status: string;
+};
