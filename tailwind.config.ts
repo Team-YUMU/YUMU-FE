ba8eba0e3c293b9c36a0c691337e9fa2128ed39f
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-03-10 14:48:40 +0900

    Shadcn/ui 설치 및 세팅 (#9)
    * feat : shadcn/ui 설치
    
    * settings : 불필요한 폴더 및 파일 정리

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,275 +1,80 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
-    screens: {
-      // => @media (max-width: 1119px) { ... }
-      md: { max: '1199px' }, // tablet
-
-      // => @media (max-width: 743px) { ... }
-      sm: { max: '743px' }, // mobile
-    },
-    fontSize: {
-      // <p class="text-12-400 ...">The quick brown fox ...</p> 처럼 사용
-      '10-400': [
-        '1rem',
-        {
-          fontWeight: '400',
-        },
-      ],
-      //fontsize: 12px
-      '12-400': [
-        '1.2rem',
-        {
-          fontWeight: '400',
-        },
-      ],
-      '12-500': [
-        '1.2rem',
-        {
-          fontWeight: '500',
-        },
-      ],
-      '12-600': [
-        '1.2rem',
-        {
-          fontWeight: '600',
-        },
-      ],
-      '12-700': [
-        '1.2rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 14px
-      '14-400': [
-        '1.4rem',
-        {
-          fontWeight: '400',
-        },
-      ],
-      '14-500': [
-        '1.4rem',
-        {
-          fontWeight: '500',
-        },
-      ],
-
-      //fontsize: 16px
-      '16-400': [
-        '1.6rem',
-        {
-          fontWeight: '400',
-        },
-      ],
-      '16-500': [
-        '1.6rem',
-        {
-          fontWeight: '500',
-        },
-      ],
-      '16-600': [
-        '1.6rem',
-        {
-          fontWeight: '600',
-        },
-      ],
-
-      //fontsize: 18px
-      '18-400': [
-        '1.8rem',
-        {
-          fontWeight: '400',
-        },
-      ],
-      '18-500': [
-        '1.8rem',
-        {
-          fontWeight: '500',
-        },
-      ],
-      '18-700': [
-        '1.8rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 20px
-      '20-500': [
-        '2rem',
-        {
-          fontWeight: '500',
-        },
-      ],
-      '20-600': [
-        '2rem',
-        {
-          fontWeight: '600',
-        },
-      ],
-      '20-700': [
-        '2rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 22px
-      '22-500': [
-        '2.2rem',
-        {
-          fontWeight: '500',
-        },
-      ],
-      '22-700': [
-        '2.2rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 24px
-      '24-700': [
-        '2.4rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 28px
-      '28-700': [
-        '2.8rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 36px
-      '36-700': [
-        '3.6rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 40px
-      '40-700': [
-        '4rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 42px
-      '42-700': [
-        '4.2rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 48px
-      '48-700': [
-        '4.8rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 56px
-      '56-700': [
-        '5.6rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 70px
-      '70-700': [
-        '7rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 76px
-      '76-700': [
-        '7.6rem',
-        {
-          fontWeight: '700',
-        },
-      ],
-
-      //fontsize: 90px
-      '90-700': [
-        '9rem',
-        {
-          fontWeight: '700',
-        },
-      ],
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
-      fontFamily: {
-        // 예시) <p className="text-pretendard">
-        pretendard: ['Pretendard Variable'],
-      },
       colors: {
-        // 예시) <p className="bg-gray-D">
-        black: {
-          0: '#000000',
-          1: '#171717',
-          3: '#333236',
-          4: '#4B4B4B',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        gray: {
-          7: '#787486',
-          9: '#9FA6B2',
-          D: '#D9D9D9',
-          E: '#EEEEEE',
-          F: '#FAFAFA',
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        white: {
-          DEFAULT: '#FFFFFF',
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        violet: {
-          5: '#5534DA',
-          F: '#F1EFFD',
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
-        red: {
-          DEFAULT: '#D6173A',
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        green: {
-          DEFAULT: '#7AC555',
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        purple: {
-          DEFAULT: '#760DDE',
-        },
-        orange: {
-          DEFAULT: '#FFA500',
-        },
-        blue: {
-          DEFAULT: '#76A5EA',
-        },
-        pink: {
-          DEFAULT: '#E876EA',
-          F: '#F4D7DA',
-          D: '#D25B68',
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
       },
-      scale: {
-        103: '1.03',
-        104: '1.04',
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require('tailwind-scrollbar-hide')],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
