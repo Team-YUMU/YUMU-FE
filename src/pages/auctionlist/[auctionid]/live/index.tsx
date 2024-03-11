b3ce56f074f78d9c6e09718e3ea3f71cdda868b5
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-03-11 19:07:54 +0900

    Live 경매(구매자) 페이지 와이어 프레임 작성 (#20)
    * feat: 필요한 shadcn/ui 설치
    
    * feat : shadc/ui 컴포넌트 배치
    
    * feat : 경매 현황 작성
    
    * feat : 컴포넌트화 1 : 채팅란
    
    * chore : 필요한 컴포넌트들 설치
    
    * feat : 컴포넌트화 2 : 비디오
    
    * feat : 컴포넌트화 3 : 경매 현황
    
    * feat : 컴포넌트화 4 : 캐러셀
    
    * feat : 컴포넌트화 5 : 경매 상세
    - 로렌 입숨 사용
    - 메뉴탭, 스크롤 이동 두 가지 버전으로 준비
    
    * feat : 라이브 경매 페이지에 컴포넌트 적용
    
    * refactor : 폴더 및 파일 구조 변경
    
    * design : prettier 룰 적용

--- src/pages/auctionlist/[auctionid]/live/index.tsx
+++ src/pages/auctionlist/[auctionid]/live/index.tsx
@@ -1,3 +1,31 @@
+import { LiveBids } from '@/components/domain/live/livebids';
+import { ExhibitionCarousel } from '@/components/common/ExhibitionCarousel';
+import { LiveChatting } from '@/components/domain/live/livechatting';
+import { AuctionDetailContents } from '@/components/common/AuctionDetailContents';
+import { LiveDetailScroll } from '@/components/domain/live/livedetailscroll';
+import { LiveVideo } from '@/components/domain/live/livevideo';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+
 export default function Live() {
-  return <div>Live</div>;
+  return (
+    <div className='m-auto flex h-dvh select-none flex-col items-center gap-2'>
+      <div className='w-full'>Header</div>
+      <div className='grid grid-cols-3 gap-2 px-2'>
+        <div className='col-span-3 bg-slate-200'>상품명</div>
+        {/* <LiveVideo /> */}
+        <div className='col-span-2'>
+          <AspectRatio ratio={16 / 9}>
+            비디오 켜지면 플리 이상하게 들려서 끄려고 대충 만든 녀석인데 비디오 컴포넌트 잘 만들어져 있으니까 걱정
+            마세요. 사이즈도 동일합니다.
+          </AspectRatio>
+        </div>
+        <LiveChatting />
+        <LiveDetailScroll />
+        {/* <AuctionDetailContents /> */}
+        <LiveBids />
+        <ExhibitionCarousel />
+        <div className='w-full'>Footer</div>
+      </div>
+    </div>
+  );
 }
