57d378fec8e7cd77a64f802fffba9b4be1a70564
Sungeunji <danceunji@gmail.com>
2024-03-11 23:54:30 +0900

    feat: types폴더에에 validation.ts 파일 생성.

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,27 +3,9 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import axios from 'axios';
+import { schema } from '@/types/validation';
 
-const schema = z
-  .object({
-    email: z
-      .string()
-      .email({ message: '유효한 이메일 주소를 입력하세요.' })
-      .min(1, { message: '이메일을 입력해주세요.' }),
-    password: z
-      .string()
-      .min(6, { message: '비밀번호는 최소 6자 이상이어야 합니다.' })
-      .regex(/[A-Za-z]/, { message: '비밀번호는 영문 대소문자를 포함해야 합니다.' }),
-    passwordCheck: z.string(),
-  })
-  .refine(
-    (value) => {
-      return value.password === value.passwordCheck;
-    },
-    { message: '비밀번호가 일치하지 않습니다.', path: ['passwordCheck'] },
-  );
-
-export default function SignUpPage() {
+export function SignUpPage() {
   const router = useRouter();
 
   type FormData = {
