503f02f30fbd307d8b26ba1564e8d4775aebe199
박지원 <jackgg12322@gmail.com>
2024-03-13 03:57:28 +0900

    feat : validation 정규표현식 수정 및 추가

--- src/types/validation.ts
+++ src/types/validation.ts
@@ -8,9 +8,14 @@ export const schema = z
       .min(1, { message: '이메일을 입력해주세요.' }),
     password: z
       .string()
-      .min(6, { message: '비밀번호는 최소 6자 이상이어야 합니다.' })
-      .regex(/[A-Za-z]/, { message: '비밀번호는 영문 대소문자를 포함해야 합니다.' }),
+      .min(6, { message: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.' })
+      .regex(/^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/, { message: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.' }),
     passwordCheck: z.string(),
+    nickname: z
+      .string()
+      .min(2, { message: '닉네임은 최소 2자 이상 최대 10자 까지 가능합니다.' })
+      .max(10, { message: '닉네임은 최소 2자 이상 최대 10자 까지 가능합니다.' })
+      .regex(/^\S[a-zA-Z0-9 ]*$/, { message: '공백이 존재합니다.' }),
   })
   .refine(
     (value) => {
