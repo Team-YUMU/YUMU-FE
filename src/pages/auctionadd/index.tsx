377a21920458f4714088178f2117a8c7405cfcf5
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-03-14 18:52:19 +0900

    경매 등록 페이지 1차 와이어프레임 기반 작성 (#49)
    * Mockup 데이터 구축 (#5)
    
    * Add gitignore
    
    * Update issue templates
    
    * test page 구현 테스트
    
    * test: 배포테스트
    
    * test: 배포 테스트 재도전
    
    * 메인 index 컴포넌트 수정, 배포재도전
    
    * fix : eslint rule 수정
    
    * chore : test
    
    * feat : types.ts에 사용자, 구매내역, 판매 내역, 관심 상품, 경매 게시물 추가
    
    * feat : 임시 Mockup Data 추가 (Auction, Member, PurchaseHistory, SalesHistory, WishList)
    
    * refactor : 타입명 변경 (Member -> MemberType)
    
    ---------
    
    Co-authored-by: Sungeunji <danceunji@gmail.com>
    Co-authored-by: 박지원 <jackgg12322@gmail.com>
    
    * feat : 경매 등록 페이지 일부 완성
    - 작품 제목, 작품 이미지, 작품 상세, 경매 시작 일시, 경매 시작가, 배송 방법, 유의사항
    
    * Merge branch 'develop' of https://github.com/Team-YUMU/YUMU-FE into feature/00TaciTa00/24
    
    * feat : artSize, 이미지 미리보기 기능 추가
    
    * feat : datetimepicker 적용 및 작품 규모에 정규 표현식 적용
    
    * feat : datetimepicker 적용 및 작품 규모에 정규 표현식 적용
    
    * fix : auction type error
    
    ---------
    
    Co-authored-by: Sungeunji <danceunji@gmail.com>
    Co-authored-by: 박지원 <jackgg12322@gmail.com>

---
+++ src/pages/auctionadd/index.tsx
@@ -0,0 +1,297 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CalendarIcon } from 'lucide-react';
+import { format } from 'date-fns';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Separator } from '@/components/ui/separator';
+import { Textarea } from '@/components/ui/textarea';
+import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from '@/components/ui/select';
+import { auctionFormSchema } from '@/types/validation';
+import { useState } from 'react';
+import Image from 'next/image';
+import { TimePickerDemo } from '@/components/domain/live/time-picker-demo';
+
+type AuctionFormTypes = z.infer<typeof auctionFormSchema>;
+
+const defaultValues: Partial<AuctionFormTypes> = {
+  artTitle: '',
+  artImage: '',
+  artDetail: '',
+  artSize: '',
+  startDate: new Date(0),
+  endDate: new Date(0),
+  startPrice: 0,
+  receiveType: '',
+  notice: '',
+};
+
+export default function AuctionAdd() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const form = useForm<AuctionFormTypes>({
+    resolver: zodResolver(auctionFormSchema),
+    defaultValues,
+  });
+
+  function onSubmit(data: AuctionFormTypes) {
+    console.log(data);
+  }
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(String(reader.result));
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className='m-auto w-[120rem] pt-2'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+          <FormField
+            control={form.control}
+            name='artTitle'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>작품 제목</FormLabel>
+                </div>
+                <div>
+                  <FormControl>
+                    <Input placeholder='작품 제목' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <Separator />
+          <FormField
+            control={form.control}
+            name='artImage'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>대표 이미지</FormLabel>
+                </div>
+                <div className='space-y-2'>
+                  {selectedImage && (
+                    <div className='relative h-[10rem]'>
+                      <Image src={selectedImage} alt='Preview' fill style={{ objectFit: 'contain' }} />
+                    </div>
+                  )}
+                  <FormControl>
+                    <Input
+                      type='file'
+                      placeholder='대표 이미지'
+                      onInput={handleFileChange}
+                      accept='image/*'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <Separator />
+          <FormField
+            control={form.control}
+            name='artDetail'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>작품 상세</FormLabel>
+                </div>
+                <div>
+                  <FormControl>
+                    <Textarea placeholder='작품 상세' className='resize-none' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <Separator />
+          <FormField
+            control={form.control}
+            name='artSize'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>작품 규모</FormLabel>
+                </div>
+                <div>
+                  <FormControl>
+                    <Input placeholder='가로 x 세로 x 높이' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <Separator />
+          <FormField
+            control={form.control}
+            name='startDate'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>경매 기간</FormLabel>
+                </div>
+                <div>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={'outline'}
+                        className={cn(
+                          'w-[280px] justify-start text-left font-normal',
+                          !field.value && 'text-muted-foreground',
+                        )}
+                      >
+                        <CalendarIcon className='mr-2 h-4 w-4' />
+                        {field.value ? format(field.value, 'yyyy-MM-dd HH:mm:ss') : <span>경매 시작 일시</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className='w-auto p-0'>
+                      <Calendar
+                        mode='single'
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date < new Date()}
+                        initialFocus
+                      />
+                      <div className='border-t border-border p-3'>
+                        <TimePickerDemo setDate={field.onChange} date={field.value} />
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='endDate'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div />
+                <div>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={'outline'}
+                        className={cn(
+                          'w-[280px] justify-start text-left font-normal',
+                          !field.value && 'text-muted-foreground',
+                        )}
+                      >
+                        <CalendarIcon className='mr-2 h-4 w-4' />
+                        {field.value ? format(field.value, 'yyyy-MM-dd HH:mm:ss') : <span>경매 종료 일시</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className='w-auto p-0'>
+                      <Calendar
+                        mode='single'
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date < new Date()}
+                        initialFocus
+                      />
+                      <div className='border-t border-border p-3'>
+                        <TimePickerDemo setDate={field.onChange} date={field.value} />
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          {/* <input type='date' />
+          <input type='time' />
+          <input type='datetime-local' /> */}
+          <Separator />
+          <FormField
+            control={form.control}
+            name='startPrice'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>경매 시작가</FormLabel>
+                </div>
+                <div>
+                  <FormControl>
+                    <Input type='number' placeholder='경매 시작가' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <Separator />
+          <FormField
+            control={form.control}
+            name='receiveType'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>수령 방법</FormLabel>
+                </div>
+                <div>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder='수령 방법' />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value='대면수령'>대면 수령</SelectItem>
+                      <SelectItem value='택배배송'>택배 배송</SelectItem>
+                      <SelectItem value='이메일 수신'>이메일 수신</SelectItem>
+                      <SelectItem value='기타'>기타</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </FormItem>
+            )}
+          />
+          <Separator />
+          <FormField
+            control={form.control}
+            name='notice'
+            render={({ field }) => (
+              <FormItem className='grid w-full grid-cols-[2fr_3fr] '>
+                <div>
+                  <FormLabel>유의 사항</FormLabel>
+                </div>
+                <div>
+                  <FormControl>
+                    <Textarea placeholder='유의 사항' className='resize-none' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className='float-right'>
+            경매 등록
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
