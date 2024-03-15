3e9a10180dc4e894c09906ddb58520cb0af2c88d
정현진 <jhj1004v@naver.com>
2024-03-15 16:12:37 +0900

    design : 배너 ui 작업 (ing)

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,7 +1,110 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+
 export default function Landing() {
   return (
     <>
-      <h1>랜딩</h1>
+      <main className='container'>
+        {/* 베너 버튼 없애고 자동응로 넘어가도록 구현 */}
+        <div>
+          <Carousel className=' w-full max-w-xs'>
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className='p-1'>
+                    <Card>
+                      <CardContent className='flex aspect-square items-center justify-center p-6'>
+                        <span className='text-4xl font-semibold'>{index + 1}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+        {/* 현재 라이브 경매 버튼없음*/}
+        <div>현재 Live 경매</div>
+        <Carousel className=' w-full max-w-xs'>
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className='p-1'>
+                  <Card>
+                    <CardContent className='flex aspect-square items-center justify-center p-6'>
+                      <span className='text-4xl font-semibold'>{index + 1}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        {/* 기획전 */}
+        <div>
+          <Carousel className=' w-full max-w-xs'>
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className='p-1'>
+                    <Card>
+                      <CardContent className='flex aspect-square items-center justify-center p-6'>
+                        <span className='text-4xl font-semibold'>{index + 1}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+        {/* 오픈예정 */}
+        <div>
+          <Carousel className=' w-full max-w-xs'>
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className='p-1'>
+                    <Card>
+                      <CardContent className='flex aspect-square items-center justify-center p-6'>
+                        <span className='text-4xl font-semibold'>{index + 1}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+        {/* 인기경매 */}
+        <div>
+          <Carousel className=' w-full max-w-xs'>
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className='p-1'>
+                    <Card>
+                      <CardContent className='flex aspect-square items-center justify-center p-6'>
+                        <span className='text-4xl font-semibold'>{index + 1}</span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </main>
     </>
   );
 }
