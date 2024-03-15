a5ca6b312aa8e1c05be5ecc183d8fff523f7660f
박지원 <jackgg12322@gmail.com>
2024-03-16 01:28:49 +0900

    feat : test password 활용

--- src/types/newPwdValidation.ts
+++ src/types/newPwdValidation.ts
@@ -9,13 +9,18 @@ function mypagePassWordSchema() {
     });
 }
 
-export type TMypagePwResetSchema = z.infer<typeof myPagePwResetSchema>;
+export type TMypagePwEditSchema = z.infer<typeof myPagePwEditSchema>;
+const userPwData = 'Qwer!234';
 
-export const myPagePwResetSchema = z
+export const myPagePwEditSchema = z
   .object({
     password: mypagePassWordSchema(),
     newPassword: mypagePassWordSchema(),
     newPasswordCheck: mypagePassWordSchema(),
+  })
+  .refine((data) => userPwData === data.password, {
+    path: ['password'],
+    message: '현재 비밀번호와 일치하지 않습니다.',
   })
   .refine((data) => data.newPassword === data.newPasswordCheck, {
     path: ['newPasswordCheck'],
