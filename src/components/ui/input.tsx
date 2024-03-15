714458cfec09067b3941266206a2039002af588b
Park Ji Won <124119421+PJW980921@users.noreply.github.com>
2024-03-15 12:19:31 +0900

    authInput 컴포넌트 수정 (#51)
    * remove : react-icons 라이브러리 삭제
    
    * setting : 아이콘 컴포넌트 파일 명 수정
    
    * fix : authInput onBlur 함수 수정
    
    * fix : react-icons 라이브러리 설치

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
     <input
       type={type}
       className={cn(
-        `placeholder:text-gray-B focus:outline-orange-F file:text-sm container h-[5rem] rounded-[0.8rem] border-[0.1rem] bg-gray-F px-[4rem] py-[1.5rem] text-16-400 outline-gray-E file:border-0 file:bg-transparent file:font-medium focus:bg-white focus-visible:ring-offset-2`,
+        `file:text-sm container h-[5rem] rounded-[0.8rem] border-[0.1rem] bg-gray-F px-[4rem] py-[1.5rem] text-16-400 outline-gray-E file:border-0 file:bg-transparent file:font-medium placeholder:text-gray-B focus:bg-white focus:outline-orange-F focus-visible:ring-offset-2`,
         className,
       )}
       ref={ref}
