d8984a5687cc420d0ef3731d64f151046c1dadad
박지원 <jackgg12322@gmail.com>
2024-03-16 21:48:43 +0900

    refactor : Tabs active State 관리

--- src/components/domain/myPage/Tabs/MyPageTabs.tsx
+++ src/components/domain/myPage/Tabs/MyPageTabs.tsx
@@ -2,19 +2,40 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import BuyHistory from './BuyHistory';
 import SalesHistory from './SalesHistory';
 import WishList from './WishList';
+import { useState } from 'react';
 export default function MyPageTabs() {
+  const [activeTab, setActiveTab] = useState('buy history');
+
   const tabStyle =
     'border-gray-99 border-b-2 text-gray-D hover:border-b-2 hover:border-red-F hover:text-red-F focus:border-red-F focus:text-red-F';
   return (
-    <Tabs defaultValue='account' className='flex flex-col gap-[3rem]'>
+    <Tabs value={activeTab} defaultValue='account' className='flex flex-col gap-[3rem]'>
       <TabsList className='text-15-400 flex flex-row bg-white '>
-        <TabsTrigger className={`${tabStyle}`} value='buy history'>
+        <TabsTrigger
+          onClick={() => {
+            setActiveTab('buy history');
+          }}
+          className={`${tabStyle}`}
+          value='buy history'
+        >
           구매내역
         </TabsTrigger>
-        <TabsTrigger className={`${tabStyle}`} value='sales history'>
+        <TabsTrigger
+          onClick={() => {
+            setActiveTab('sales history');
+          }}
+          className={`${tabStyle}`}
+          value='sales history'
+        >
           판매내역
         </TabsTrigger>
-        <TabsTrigger className={`${tabStyle}`} value='wish list'>
+        <TabsTrigger
+          onClick={() => {
+            setActiveTab('wish list');
+          }}
+          className={`${tabStyle}`}
+          value='wish list'
+        >
           관심작품
         </TabsTrigger>
       </TabsList>
