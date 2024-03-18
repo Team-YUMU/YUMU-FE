3825a8a34be4b1560bddae04225cd69194fe0bda
정현진 <jhj1004v@naver.com>
2024-03-18 19:57:38 +0900

    remove : slick 으로 만든 컴포넌트 제거

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,49 +1,13 @@
+import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 
 export default function Landing() {
   return (
     <>
-      <main className='container'>
-        {/* 베너 버튼 없애고 자동응로 넘어가도록 구현 */}
-        <div>
-          <Carousel className=' w-full max-w-xs'>
-            <CarouselContent>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index}>
-                  <div className='p-1'>
-                    <Card>
-                      <CardContent className='flex aspect-square items-center justify-center p-6'>
-                        <span className='text-4xl font-semibold'>{index + 1}</span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
-        {/* 현재 라이브 경매 버튼없음*/}
-        <div>현재 Live 경매</div>
-        <Carousel className=' w-full max-w-xs'>
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index}>
-                <div className='p-1'>
-                  <Card>
-                    <CardContent className='flex aspect-square items-center justify-center p-6'>
-                      <span className='text-4xl font-semibold'>{index + 1}</span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+      <main className='mx-auto max-w-[137.5rem] bg-[pink]'>
+        {/* 현재 라이브 경매 - 정적a */}
+        {/* 인기경매 - 정적*/}
         {/* 기획전 */}
         <div>
           <Carousel className=' w-full max-w-xs'>
