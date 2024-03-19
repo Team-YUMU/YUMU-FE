350ee3e58ba9373496edc70d9c3a59649b5fc547
박지원 <jackgg12322@gmail.com>
2024-03-19 16:15:02 +0900

    refactor : 불필요한 선언 제거

--- src/components/domain/myPage/Tabs/BuyHistory.tsx
+++ src/components/domain/myPage/Tabs/BuyHistory.tsx
@@ -1,5 +1,4 @@
 import PurchaseHistories from '@/mocks/PurchaseHistory';
-import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 
 export default function BuyHistory() {
