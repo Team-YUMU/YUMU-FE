2b03190729948ba096347f8c0ac5d7265efb91ea
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-03-20 18:27:47 +0900

    feat : tailwind.config에 몇가지 텍스트 스타일 추가 및 유의사항 div 수정

--- src/pages/auction/[auctionid]/streaming/index.tsx
+++ src/pages/auction/[auctionid]/streaming/index.tsx
@@ -3,18 +3,23 @@ import { LiveChatting } from '@/components/domain/live/livechatting';
 import { AuctionDetail } from '@/components/common/AuctionDetail';
 import { LiveVideo } from '@/components/domain/live/livevideo';
 import { AspectRatio } from '@/components/ui/aspect-ratio';
+import { CircleAlert } from 'lucide-react';
 
 export default function Streaming() {
   return (
-    <div className='m-auto flex w-[80%] select-none flex-col items-center gap-2 py-2'>
-      <div className='grid grid-cols-3 gap-2 px-2'>
-        <div className='col-span-3 bg-slate-200'>상품명</div>
-        {/* <LiveVideo /> */}
-        <div className='col-span-2'>
-          <AspectRatio ratio={16 / 9}>
+    <div className='m-auto flex w-[138rem] select-none flex-col items-center gap-2 py-2'>
+      <div className='grid grid-cols-3 gap-[1.25rem] px-2'>
+        <div className='text-36-400 col-span-3 bg-slate-200'>상품명(긴제목)</div>
+        <div className='col-span-2 flex w-[91rem] flex-col gap-[1.13rem]'>
+          <div className='h-[54rem] w-full bg-stone-50'>
             비디오 켜지면 플리 이상하게 들려서 끄려고 대충 만든 녀석인데 비디오 컴포넌트 잘 만들어져 있으니까 걱정
             마세요. 사이즈도 동일합니다.
-          </AspectRatio>
+          </div>
+          {/* <LiveVideo /> */}
+          <div className='text-16-700 flex h-[8rem] w-full flex-row items-center space-x-[1.3rem] rounded-xl bg-stone-50 px-[3.2rem]'>
+            <CircleAlert color='#bdbdbd' size={16} />
+            <p className='text-16-700 text-stone-300'>제주도 및 도서 산간 지역은 배송이 불가합니다.</p>
+          </div>
         </div>
         <LiveChatting />
         <AuctionDetail />
