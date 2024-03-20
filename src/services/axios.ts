3c16b1d6cfa4827dcac3d6dc441320cda88f8c78
박지원 <jackgg12322@gmail.com>
2024-03-20 23:41:49 +0900

    setting : axios baseURL 설정

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://',
+  baseURL: 'http://43.200.219.117:8080/',
 });
 
 export const authInstance = axios.create({
-  baseURL: 'https://',
+  baseURL: 'http://43.200.219.117:8080/',
   headers: {
     Authorization: typeof window !== 'undefined' ? `Bearer ${localStorage.getItem('accessToken')}` : '',
   },
@@ -13,7 +13,7 @@ export const authInstance = axios.create({
 
 // image 업로드 시 사용하는 instance
 export const authInstanceWithMedia = axios.create({
-  baseURL: 'https://',
+  baseURL: 'http://43.200.219.117:8080/',
   headers: {
     'Content-Type': 'multipart/form-data',
     Authorization: typeof window !== 'undefined' ? `Bearer ${localStorage.getItem('accessToken')}` : '',
