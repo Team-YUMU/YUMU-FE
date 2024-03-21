f66ffea41c76986c60f2fd142e9f4530343f0eeb
성은지 <116065415+Nebaisgood@users.noreply.github.com>
2024-03-21 11:43:24 +0900

    AuthInput, Button 수정 (#83)
    * chore: 새 브랜치에 작업중
    
    * chore: 새 브랜치에 작업중
    
    * chore: 새 브랜치에 작업중, 회원가입페이지 ui 업데이트
    
    * chore: 새 브랜치에 작업중,로그인 페이지 ui 업데이트
    
    * chore: 회원가입, 로그인 페이지에 맞게 AuthInput, Button 컴포넌트 수정
    
    * chore: 회원가입, 로그인 페이지에 맞게 AuthInput 의 placeholder 텍스트크기 추가수정
    
    * chore: 기타수정

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 wp-10',
-        auth: 'w-[27.375rem] h-[4rem]',
+        auth: 'w-[43.8rem] h-[6.4rem]',
         header: 'w-[16rem] h-[4.8rem] bg-red-F rounded-[0.6rem]',
       },
     },
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,17 +1,29 @@
-import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+
 import axios from 'axios';
+import Link from 'next/link';
 import { schema } from '@/types/validator/signForm';
-import { Input } from '@/components/ui/input';
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogAction,
+} from '@/components/ui/alert-dialog';
+
+import AuthInput from '@/components/ui/AuthInput';
+import { Button } from '@/components/ui/button';
 
 export default function SignUpPage() {
-  const router = useRouter();
+  // const router = useRouter();
 
   type FormData = {
     email: string;
     password: string;
     passwordCheck: string;
+    nickname: string;
   };
 
   const {
@@ -20,37 +32,95 @@ export default function SignUpPage() {
     formState: { errors },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-  const onSubmit = async (data: FormData) => {
-    try {
-      await axios.post('/', data, { withCredentials: true }); // api명세나오면 알수있음
-      router.push('/');
-    } catch (error) {
-      console.log('An error occurred:', error);
-    }
-    console.log(data);
+  const onSubmit = () => {
+    console.log('onsubmit');
   };
 
+  // = async (data: FormData) => {
+  //   try {
+  //     await axios.post('/api/v1/auth/signup', data);
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log('An error occurred:', error);
+  //   }
+  //   console.log('submit');
+  // };
+
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>email</label>
-          <Input {...register('email')} placeholder='email' />
-          {errors.email && <p>{errors.email.message}</p>}
+    <AlertDialog>
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        <div className='flex w-[43.8rem]  flex-col items-center gap-6 p-10'>
+          <div className='flex flex-col items-center'>
+            <h1 className='font-jamsil text-[4.6rem]'>회원가입</h1>
+            <h2 className='font-notosans text-[1.6rem] text-gray-9'>회원가입에 필요한 정보를 입력해주세요.</h2>``
+          </div>
+          <form
+            className='flex w-full flex-col items-center justify-center gap-[4rem] '
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <AuthInput
+              type='text'
+              placeholder='닉네임을 입력해주세요'
+              className='h-[3.9rem] w-[43.8rem] placeholder:font-[2rem]'
+              required={true}
+              errorMessage={errors?.nickname?.message}
+              {...register('nickname')}
+            />
+            {errors.nickname && <p></p>}
+
+            <AuthInput
+              type='email'
+              required={!!errors.email}
+              placeholder='이메일을 입력해 주세요'
+              errorMessage={errors?.email?.message}
+              className='h-[3.9rem] w-[43.8rem]'
+              {...register('email')}
+            />
+            {errors.email && <p></p>}
+
+            <AuthInput
+              type='password'
+              required={!!errors.password}
+              errorMessage={errors?.password?.message}
+              className='h-[3.9rem] w-[43.8rem]'
+              {...register('password')}
+              placeholder='비밀번호를 입력해주세요'
+            />
+            {errors.password && <p></p>}
+
+            <AuthInput
+              type='password'
+              required={!!errors.passwordCheck}
+              errorMessage={errors?.passwordCheck?.message}
+              className='h-[3.9rem] w-[43.8rem]'
+              {...register('passwordCheck')}
+              placeholder='비밀번호를 한번 더 적어주세요'
+            />
+            <AlertDialogTrigger>
+              <Button variant='default' className=' mt-[4rem] bg-red-F text-[2rem]' type='submit' size='auth'>
+                회원가입
+              </Button>
+            </AlertDialogTrigger>
+          </form>
+          <div className='flex gap-4 text-[1.4rem]'>
+            <h1 className='font-noto-sans-kr  text-gray-9'>이미 유무 회원이신가요?</h1>
+            <Link className=' font-apple-sd-gothic-neo-m00 text-red-F underline' href='/signin'>
+              로그인하기
+            </Link>
+          </div>
         </div>
-        <div>
-          <label>password</label>
-          <Input type='password' {...register('password')} placeholder='password' />
-          {errors.password && <p>{errors.password.message}</p>}
-        </div>
-        <div>
-          <label>password check</label>
-          <Input type='password' {...register('passwordCheck')} placeholder='password confirm' />
-          {errors.passwordCheck && <p>{errors.passwordCheck.message}</p>}
-        </div>
-        <button type='submit'>Sign Up</button>
-      </form>
-    </div>
+
+        <AlertDialogContent className='flex h-[17.9rem] flex-col items-center justify-around p-0'>
+          <div className='mt-[5rem]  '>
+            <AlertDialogHeader>
+              <AlertDialogTitle className='text-[1.6rem]'>회원가입이 완료되었습니다</AlertDialogTitle>
+            </AlertDialogHeader>
+            <AlertDialogAction className='rounded-[0.8rem mt-[4.1rem] h-[5rem] w-[32rem] border-t-2 bg-white text-[2rem] text-red-F hover:bg-white'>
+              닫기
+            </AlertDialogAction>
+          </div>
+        </AlertDialogContent>
+      </div>
+    </AlertDialog>
   );
 }
