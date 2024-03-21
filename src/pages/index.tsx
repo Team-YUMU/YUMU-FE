722b8cf7d5d06922eb6969bb18a2a4dce4264744
Sungeunji <danceunji@gmail.com>
2024-03-21 10:35:48 +0900

    chore: 새 브랜치에 작업중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Landing from './landing';
-
+import SignInPage from './signup';
 export default function Home() {
   return (
     <>
       <Landing />
+      <SignInPage />
     </>
   );
 }
