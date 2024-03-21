9d5f38851c61a57b531beac83b2ba56079cd3ee1
박지원 <jackgg12322@gmail.com>
2024-03-21 17:59:09 +0900

    setting : proxy 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    // 아래를 추가합니다.
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
@@ -24,6 +22,13 @@ const nextConfig = {
       },
     ],
   },
+  redirects: async () => [
+    {
+      source: '/api/v1/:path*',
+      destination: 'http://43.200.219.117:8080/api/v1/:path*',
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
