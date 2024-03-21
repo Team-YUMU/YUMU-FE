da67bc2b0de6cfd27b08d30f0ab92810663cb77e
Sungeunji <danceunji@gmail.com>
2024-03-21 10:41:49 +0900

    chore: 새 브랜치에 작업중, 회원가입페이지 ui 업데이트

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,19 +1,29 @@
-import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { schema } from '@/types/validator/signForm';
-import AuthInput from '@/components/ui/AuthInput';
-import { postAuthLogin } from '@/services/api';
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import axios from 'axios';
 
-export default function SignInPage() {
-  const router = useRouter();
+import axios from 'axios';
+import Link from 'next/link';
+import { schema } from '@/types/validator/signForm';
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogAction,
+} from '@/components/ui/alert-dialog';
+
+import AuthInput from '@/components/ui/AuthInput';
+import { Button } from '@/components/ui/button';
+
+export default function SignUpPage() {
+  // const router = useRouter();
 
   type FormData = {
     email: string;
     password: string;
+    passwordCheck: string;
+    nickname: string;
   };
 
   const {
@@ -23,99 +33,94 @@ export default function SignInPage() {
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = async (data: FormData) => {
-    const { email, password } = data;
-    console.log(data);
     try {
-      await postAuthLogin({ email, password });
+      await axios.post('/api/v1/auth/signup', data);
       console.log(data);
-      router.push('/');
     } catch (error) {
-      if (axios.isAxiosError(error) && error.response?.status === 404) {
-        console.log('가입되지 않은 사용자 입니다.');
-        console.log('axios 에러..!');
-      } else {
-        console.log('로그인 에러', error);
-      }
+      console.log('An error occurred:', error);
     }
-    console.log('button clicked');
-  };
-
-  const client_id = '2ddd4adce2c2202ed86dcf98ce65602a';
-  const redirect_uri = 'http://localhost:3000/';
-  const response_type = 'code';
-
-  const handleKakaoLogin = async () => {
-    const authParam = new URLSearchParams({
-      client_id,
-      redirect_uri,
-      response_type,
-    });
-    const KAKAO_BASE_URL = `https://kauth.kakao.com/oauth/authorize?${authParam.toString()}`;
-    router.push(KAKAO_BASE_URL);
+    console.log('submit');
   };
 
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center'>
-      <div className='0 flex w-[43.8rem] flex-col items-center gap-[4.8rem] p-10'>
-        <div className='flex flex-col items-center'>
-          <h1 className='font-jamsil text-[4.6rem]'>로그인</h1>
-          <h2 className='font-notosans text-[1.6rem] text-gray-9'>YUMU에 방문해주셔서 감사합니다.</h2>
+    <AlertDialog>
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        <div className='flex w-[43.8rem]  flex-col items-center gap-6 p-10'>
+          <div className='flex flex-col items-center'>
+            <h1 className='font-jamsil text-[4.6rem]'>회원가입</h1>
+            <h2 className='font-notosans text-[1.6rem] text-gray-9'>회원가입에 필요한 정보를 입력해주세요.</h2>``
+          </div>
+          <form
+            className='flex w-full flex-col items-center justify-center gap-[2rem] '
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <AuthInput
+              formNoValidate
+              type='text'
+              placeholder='닉네임을 입력해주세요'
+              className=' h-[3.9rem] w-[43.8rem]'
+              required={true}
+              errorMessage={errors?.nickname?.message}
+              {...register('nickname')}
+            />
+            {errors.nickname && <p></p>}
+
+            <AuthInput
+              formNoValidate
+              type='email'
+              required={!!errors.email}
+              placeholder='이메일을 입력해 주세요'
+              errorMessage={errors?.email?.message}
+              className='h-[3.9rem] w-[43.8rem]'
+              {...register('email')}
+            />
+            {errors.email && <p></p>}
+
+            <AuthInput
+              formNoValidate
+              type='password'
+              required={!!errors.password}
+              errorMessage={errors?.password?.message}
+              className='h-[3.9rem] w-[43.8rem]'
+              {...register('password')}
+              placeholder='비밀번호를 입력해주세요'
+            />
+            {errors.password && <p></p>}
+
+            <AuthInput
+              formNoValidate
+              type='password'
+              required={!!errors.passwordCheck}
+              errorMessage={errors?.passwordCheck?.message}
+              className='h-[3.9rem] w-[43.8rem]'
+              {...register('passwordCheck')}
+              placeholder='비밀번호를 한번 더 적어주세요'
+            />
+            <AlertDialogTrigger>
+              <Button variant='default' className=' mt-[4rem] bg-red-F text-[2rem]' type='submit' size='auth'>
+                회원가입
+              </Button>
+            </AlertDialogTrigger>
+          </form>
+          <div className='flex gap-4 text-[1.4rem]'>
+            <h1 className='font-noto-sans-kr  text-gray-9'>이미 유무 회원이신가요?</h1>
+            <Link className=' font-apple-sd-gothic-neo-m00 text-red-F underline' href='/signin'>
+              로그인하기
+            </Link>
+          </div>
         </div>
 
-        <form
-          className={`flex w-full flex-col items-center justify-center ${errors.email ? 'gap-[4.5rem]' : 'gap-[1.5rem]'}`}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <AuthInput
-            type='email'
-            required={!!errors.email}
-            placeholder='이메일을 입력해 주세요'
-            errorMessage={errors?.email?.message}
-            className=' h-[3.9rem] w-[43.8rem]'
-            {...register('email')}
-          />
-
-          <AuthInput
-            type='password'
-            required={!!errors.password}
-            errorMessage={errors?.password?.message}
-            className='h-[3.9rem] w-[43.8rem]'
-            {...register('password')}
-            placeholder='비밀번호를 입력해주세요'
-          />
-
-          <div className='mt-[2.8rem] flex flex-col gap-[1.2rem]'>
-            <Button variant='default' className=' bg-red-F text-[2rem]' type='submit' size='auth'>
-              로그인
-            </Button>
-            <div className='relative'>
-              <Image
-                src='/svgs/kakao-icon.svg'
-                height={25}
-                width={28}
-                alt='kakao'
-                className='absolute left-[2.8rem] mt-8'
-              />
-              <Button
-                variant='sns'
-                size='auth'
-                className='bg-yellow text-[2rem] text-black-0'
-                onClick={handleKakaoLogin}
-              >
-                카카오로 로그인
-              </Button>
-            </div>
-            <Button
-              variant='outline'
-              size='auth'
-              className='border border-red-F bg-white text-[2rem] text-red-F'
-              onClick={() => router.push('/signup')}
-            >
-              회원가입
-            </Button>
+        <AlertDialogContent className='flex h-[17.9rem] flex-col items-center justify-around p-0'>
+          <div className='mt-[5rem]  '>
+            <AlertDialogHeader>
+              <AlertDialogTitle className='text-[1.6rem]'>회원가입이 완료되었습니다</AlertDialogTitle>
+            </AlertDialogHeader>
+            <AlertDialogAction className='rounded-[0.8rem mt-[4.1rem] h-[5rem] w-[32rem] border-t-2 bg-white text-[2rem] text-red-F hover:bg-white'>
+              닫기
+            </AlertDialogAction>
           </div>
-        </form>
+        </AlertDialogContent>
       </div>
-    </div>
+    </AlertDialog>
   );
 }
