d1d03fc7886396f0cfbdce5cd1bd3428457e457c
Sungeunji <danceunji@gmail.com>
2024-03-21 14:03:52 +0900

    chore: 기타수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import Test from './test/test';
-import Edit from '@/components/domain/myPage/Tabs/Edit/Edit';
+import Landing from './landing';
 export default function Home() {
   return (
     <>
-      <Test />
-      <Edit />
+      <Landing />
     </>
   );
 }
