c06d4675367c7b8c0cf713389878b0f741cb8f22
김윤미 <younmeet@gmail.com>
2024-03-22 14:59:15 +0900

    feat: 검색 페이지 기능 테스트 구현

--- src/components/common/AuctionCard.tsx
+++ src/components/common/AuctionCard.tsx
@@ -2,10 +2,26 @@ import Image from 'next/image';
 import { Card, CardDescription, CardTitle } from '@/components/ui/card';
 import { ArtProps } from '@/types/types';
 
-export default function AuctionCard({ artName, artImage, artist, wishCnt }: ArtProps) {
+// 테스트용
+type Art = {
+  id: number;
+  artName: string;
+  artImage: string;
+  artist: string;
+  status: string;
+  createdAt: number;
+  wishCnt: number;
+};
+
+function formatDate(value: number) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+export default function AuctionCard({ artName, artImage, artist, createdAt, wishCnt }: Art) {
   return (
     <Card className='rounded-none border-none shadow-none'>
-      <div className='group relative aspect-card-image w-full overflow-hidden'>
+      <div className='group relative aspect-card-image w-full overflow-hidden bg-gray-100 '>
         <Image fill src={artImage} alt={artName} className='transition-transform group-hover:scale-125' />
       </div>
       <div className='py-7'>
@@ -13,6 +29,7 @@ export default function AuctionCard({ artName, artImage, artist, wishCnt }: ArtP
         <CardDescription className='truncate text-14-400'>{artist}</CardDescription>
       </div>
       <div>
+        <span>{formatDate(createdAt)}</span> <br />
         <span>찜{wishCnt}</span>
       </div>
     </Card>
