fb994c67da207e520c1655bc9e9dbbb1f5b0afae
hyun522 <134246845+hyun522@users.noreply.github.com>
2024-03-25 13:57:07 +0900

    Feat/hyun522/77 main 페이지 현재라이브경매 / 인기경매 구현  (#121)
    * fix : text size 수정
    
    * chore : 현재 live경매에 타임 ui  svg
    
    * fix : variant로 style 적용 하기
    
    * design :  현재 라이브 경매  ui  구현
    
    * chore : add image next Config(landing 경매)
    
    * exhibitions 이미지 추가
    
    * feat : 데이터 동적으로 작업
    
    * rename : landing 페이지의 bannerCarousel 컴포넌트화
    
    * chore : inter 폰트 추가
    
    * feat : 현재 Live 경매 인기 Live 경매 작업

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,13 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      //landing 현재 라이브 경매 이미지
+      {
+        protocol: 'https',
+        hostname: 'yumu-image.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   redirects: async () => [
