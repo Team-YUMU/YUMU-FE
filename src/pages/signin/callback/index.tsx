344ff1bc2c030825f539d10d1d44510ce9d93c1e
Sungeunji <danceunji@gmail.com>
2024-03-28 12:48:18 +0900

    feat: 콜백페이지 생성

--- src/pages/signin/callback/index.tsx
+++ src/pages/signin/callback/index.tsx
@@ -1,3 +1,26 @@
+// import { useEffect } from 'react';
+// import axios from 'axios';
+// import { getToken } from '@/services/api';
+// import { useRouter } from 'next/router';
+
 export default function callback() {
-  return <>callback page</>;
+  return <>callback</>;
 }
+// const router = useRouter();
+// useEffect(() => {
+//   const search = new URLSearchParams(window.location.search);
+//   const code = search.get('code');
+
+//   if (code) {
+//     handleGetToken();
+//   }
+// }, []);
+
+// const handleGetToken = async () => {
+//   const { token_type, access_token, expires_in, refresh_token, refresh_token_expires_in } = await getToken();
+
+//   localStorage.setItem('access_token', access_token);
+//   localStorage.setItem('refresh', refresh_token);
+// };
+
+// router.push('/');
