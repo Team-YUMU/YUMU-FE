1b8d91a7fe4e921e5302e868209621c0547bd152
박지원 <jackgg12322@gmail.com>
2024-03-29 16:01:33 +0900

    fix : 토큰 저장 헤더 프로필 조회 버그  해결

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,16 @@
 import {
   RegistrationProps,
-  UserInfoProps,
+  MemberInfoProps,
   AuctionCheckProps,
   PostAuthLoginProps,
   PostAuthSignUpProps,
-  PutUserProps,
+  PutMemberPwdProps,
 } from '@/types/types';
-import { authInstance as authAxios, instance as axios, authInstanceForRegist as authRegist } from './axios';
+import saveTokensLocally, {
+  authInstance as authAxios,
+  instance as axios,
+  authInstanceWithMedia as axiosMedia,
+} from './axios';
 const BASE_URL = `/api/v1`;
 const MY_PAGE_BASE_URL = `${BASE_URL}/mypage`;
 const MY_PAGE_MEMBERS_URL = `${BASE_URL}/member`;
@@ -20,6 +24,8 @@ export async function postAuthLogin(loginData: PostAuthLoginProps) {
     const refreshToken = res.headers['refresh'];
     sessionStorage.setItem('accessToken', accessToken);
     sessionStorage.setItem('refreshToken', refreshToken);
+    // 클라이언트 측 코드에서 토큰을 다시 저장
+    saveTokensLocally();
     return res.data;
   } catch (error) {
     console.log(error);
@@ -78,8 +84,8 @@ export const getToken = async () => {
   }
 };
 // signUp-page API (회원가입)
-export async function postUsers(signUpData: PostAuthSignUpProps) {
-  const res = await axios.post<UserInfoProps>(`${BASE_URL}/auth/signup`, signUpData);
+export async function postMember(signUpData: PostAuthSignUpProps) {
+  const res = await axios.post<MemberInfoProps>(`${BASE_URL}/auth/signup`, signUpData);
   return res.data;
 }
 
@@ -94,24 +100,64 @@ export async function getCheckNickname(nickname: string): Promise<boolean> {
   const res = await axios.get<boolean>(`${BASE_URL}/auth/signup/nickname-check`, { params: { nickname } });
   return res.data;
 }
+// 닉네임 중복 체크
+export async function getMemberNickNameCheck(nickname: string) {
+  const res = await axios.get(`${BASE_URL}/auth/signup/nickname-check?nickname=${nickname}`);
+  return res;
+}
+// 이메일 중복 체크
+export async function getMemberEmailCheck(email: string) {
+  const res = await axios.get(`${BASE_URL}/auth/signup/email-check?email=${email}`);
+  return res;
+}
 
 // Logout API (로그아웃)
 export async function postAuthLogout() {
-  const res = await authAxios.post<UserInfoProps>(`${BASE_URL}/auth/logout`);
+  const res = await authAxios.get<MemberInfoProps>(`${BASE_URL}/auth/logout`);
+  return res.data;
+}
+// 계정 삭제
+export async function deleteWithMember() {
+  const res = await authAxios.delete(`${BASE_URL}/auth/withdraw`);
   return res.data;
 }
 
-// my-page put API (회원 정보 수정)
-export async function putMemberInfo(userInfo: PutUserProps) {
-  const res = await authAxios.put<string>(`${MY_PAGE_MEMBERS_URL}`, userInfo);
-  return res;
-}
 // my-page get API (내정보 조회)
 export async function getMemberInfo() {
   const res = await authAxios.get(`${MY_PAGE_MEMBERS_URL}`);
   return res.data;
 }
 
+// my-page put API (회원 정보 수정)
+export async function putMemberIntroduceData(introduce: string) {
+  const res = await authAxios.put(`${MY_PAGE_MEMBERS_URL}/introduce?introduce=${introduce}`);
+  return res;
+}
+// 닉네임 수정
+export async function putMemberNickNameData(nickname: string) {
+  const res = await authAxios.put(`${MY_PAGE_MEMBERS_URL}/nickname?nickname=${nickname}`);
+  return res;
+}
+//이미지 수정
+export async function putMemberProfileImageData(imageFile: FormData) {
+  const res = await axiosMedia.put(`${MY_PAGE_MEMBERS_URL}/profile-image`, imageFile);
+  return res;
+}
+//이미지 삭제
+export async function deleteMemberProfileImage() {
+  const res = await axiosMedia.delete(`${MY_PAGE_MEMBERS_URL}/profile-image`);
+  return res;
+}
+
+// my-page put API (비밀번호 수정)
+
+// 비밀번호 수정
+export async function putMemberPasswordData({ password, newPassword }: PutMemberPwdProps) {
+  const res = await authAxios.put(`${MY_PAGE_MEMBERS_URL}/password`, { password, newPassword });
+  return res;
+}
+
+// my-page put API (회원 정보 수정)
 // 내 작품 조회
 export async function getArts() {
   const res = await authAxios.get(`${MY_PAGE_BASE_URL}/arts`);
@@ -137,7 +183,7 @@ export async function getWishHistory() {
 // auction post API (경매글 등록)
 export async function postAuction(auctionRegister: FormData) {
   console.log('auctionRegister', auctionRegister);
-  const res = await authRegist.post(`${AUCTION_BASE_URL}`, auctionRegister);
+  const res = await axiosMedia.post(`${AUCTION_BASE_URL}`, auctionRegister);
   return res.data;
 }
 
