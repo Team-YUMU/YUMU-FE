8fa04669334ae358fc1db7e7280d89857185d94c
박지원 <jackgg12322@gmail.com>
2024-03-30 04:42:19 +0900

    refactor : 마이페이지 비밀번호 정규표현식 수정

--- src/types/validator/myPageForm.ts
+++ src/types/validator/myPageForm.ts
@@ -4,29 +4,28 @@ function mypagePassWordSchema() {
   return z
     .string()
     .min(6, { message: '최소 6자 이상 입력해 주세요.' })
-    .regex(/^(?=.*[a-zA-Z])(?=.*[0-9]).{6,25}$/, {
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!-*]).{6,16}$/, {
       message: '비밀번호는 영문 대/소 문자,숫자,특수기호를 조합해서 사용하세요.',
     });
 }
 
 export type TMypagePwEditSchema = z.infer<typeof myPagePwEditSchema>;
-const userPwData = 'Qwer!234';
 
 export const myPagePwEditSchema = z
   .object({
     password: mypagePassWordSchema(),
     newPassword: mypagePassWordSchema(),
-    newPasswordCheck: mypagePassWordSchema(),
+    newCheckPassword: mypagePassWordSchema(),
   })
-  .refine((data) => userPwData === data.password, {
+  .refine((data) => data.password === data.password, {
     path: ['password'],
     message: '현재 비밀번호와 일치하지 않습니다.',
   })
-  .refine((data) => data.newPassword === data.newPasswordCheck, {
-    path: ['newPasswordCheck'],
+  .refine((data) => data.newPassword === data.newCheckPassword, {
+    path: ['newCheckPassword'],
     message: '비밀번호가 일치하지 않습니다.',
   })
   .refine((data) => data.password !== data.newPassword, {
     path: ['newPassword'],
-    message: '현재 비밀번호와 새 비밀번호는 같을 수 없습니다.',
+    message: '현재 비밀번호와 새 비밀번호가 일치합니다.',
   });
