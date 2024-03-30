21dff52c534a0ed418617f490c470cd024e1f4a5
박지원 <jackgg12322@gmail.com>
2024-03-31 03:25:21 +0900

    fix : div 태그 위치 수정

--- src/components/domain/myPage/Tabs/SalesHistory.tsx
+++ src/components/domain/myPage/Tabs/SalesHistory.tsx
@@ -43,78 +43,76 @@ export default function SalesHistory() {
   return (
     <>
       <div className='inline-flex h-[73rem] flex-col gap-[1.6rem] overflow-scroll' onScroll={handleScroll}>
-        <div className='inner-container'>
-          {postInfoList?.pages.map((page, index) => (
-            <Fragment key={index}>
-              {page.postList.map((item, itemIndex) => (
-                <div className={`${historyBoxStyles} items-center`} key={itemIndex}>
-                  <div className='ml-[4.8rem] flex w-[8rem] flex-col gap-[4.3rem]'>
-                    <div className='flex flex-col gap-[0.4rem]'>
-                      <span className='h-[1.8rem] w-[9.5rem] flex-shrink-0 text-18-700 text-gray-9'>{'판매 완료'}</span>
-                      <p className='h-[3.9rem] w-[24.9rem] flex-shrink-0 text-32-700 text-black-2'>{item.artTitle}</p>
-                    </div>
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button type='button' size={'myPage'} variant={'myPage'}>
-                          <span className='text-center text-16-500 text-gray-9'>낙찰자 : {item.artist}</span>
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent className='h-[49.7rem] max-w-[44rem]'>
-                        <AlertDialogDescription>
-                          <div className='flex flex-col gap-[4rem] pl-[3.6rem]'>
-                            <div className='flex flex-col gap-[0.4rem] '>
-                              <span className=' relative mt-[3rem] h-[1.8rem] w-[9.5rem] flex-shrink-0 text-18-700 text-gray-9'>
-                                거래완료
-                              </span>
-                              <span className=' h-[3.9rem] w-[24.9rem] flex-shrink-0 text-24-700 text-gray-9'>
-                                구매자정보
-                              </span>
-                            </div>
-                            <div className='flex flex-col gap-[0.4rem] '>
-                              <span className='h-[3.9rem] w-[24.9rem] flex-shrink-0 text-32-700 text-black-2'>
-                                {item.artist}
-                              </span>
-                              <span className='text-16-500 text-gray-9'>구매자 번호</span>
-                            </div>
-                            <div className='flex flex-col gap-[0.4rem] '>
-                              <span className='text-16-500 text-gray-9'>결제금액</span>
-                              <span className='text-16-500 text-gray-9'>{item.price + `원`}</span>
-                            </div>
-                            <div className='flex flex-col gap-[0.4rem] '>
-                              <span className='text-16-500 text-gray-9'>주소</span>
-                              <span className='h-[7.2rem] w-[32rem] text-16-500 text-gray-9'>
-                                서울특별자치도 서울특별자치도 서울특별자치도 서울특별자치도 서울특별자치도 (000000)
-                                105동 103호
-                              </span>
-                            </div>
+        {postInfoList?.pages.map((page, index) => (
+          <Fragment key={index}>
+            {page.postList.map((item, itemIndex) => (
+              <div className={`${historyBoxStyles} items-center`} key={itemIndex}>
+                <div className='ml-[4.8rem] flex w-[8rem] flex-col gap-[4.3rem]'>
+                  <div className='flex flex-col gap-[0.4rem]'>
+                    <span className='h-[1.8rem] w-[9.5rem] flex-shrink-0 text-18-700 text-gray-9'>{'판매 완료'}</span>
+                    <p className='h-[3.9rem] w-[24.9rem] flex-shrink-0 text-32-700 text-black-2'>{item.artTitle}</p>
+                  </div>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button type='button' size={'myPage'} variant={'myPage'}>
+                        <span className='text-center text-16-500 text-gray-9'>낙찰자 : {item.artist}</span>
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className='h-[49.7rem] max-w-[44rem]'>
+                      <AlertDialogDescription>
+                        <div className='flex flex-col gap-[4rem] pl-[3.6rem]'>
+                          <div className='flex flex-col gap-[0.4rem] '>
+                            <span className=' relative mt-[3rem] h-[1.8rem] w-[9.5rem] flex-shrink-0 text-18-700 text-gray-9'>
+                              거래완료
+                            </span>
+                            <span className=' h-[3.9rem] w-[24.9rem] flex-shrink-0 text-24-700 text-gray-9'>
+                              구매자정보
+                            </span>
                           </div>
-                        </AlertDialogDescription>
-                        <AlertDialogCancel>
-                          <Image
-                            src={'svgs/my-page-modal-close-icon.svg'}
-                            width={30}
-                            height={30}
-                            className='absolute h-[3rem] w-[3rem] flex-shrink-0'
-                            alt='모달 닫기 버튼 아이콘'
-                          />
-                        </AlertDialogCancel>
-                      </AlertDialogContent>
-                    </AlertDialog>
-                  </div>
-                  <div className='mt-[5rem] flex flex-col gap-[2.3rem]'>
-                    <div className='flex h-[3.9rem] w-[35.5rem] flex-row items-center justify-center gap-2'>
-                      <p className='flex-shrink-0 text-36-900'>{item.price + `원`}</p>
-                      <p className='text-28-500 text-gray-9'>{'낙찰'}</p>
-                    </div>
-                    <p className='ml-[3rem] h-[1.8rem] w-[27.1rem] flex-shrink-0 text-right text-18-700 text-gray-9'>
-                      {item.saleDate.slice(0, 10) + ` 경매 종료`}
-                    </p>
-                  </div>
+                          <div className='flex flex-col gap-[0.4rem] '>
+                            <span className='h-[3.9rem] w-[24.9rem] flex-shrink-0 text-32-700 text-black-2'>
+                              {item.artist}
+                            </span>
+                            <span className='text-16-500 text-gray-9'>구매자 번호</span>
+                          </div>
+                          <div className='flex flex-col gap-[0.4rem] '>
+                            <span className='text-16-500 text-gray-9'>결제금액</span>
+                            <span className='text-16-500 text-gray-9'>{item.price + `원`}</span>
+                          </div>
+                          <div className='flex flex-col gap-[0.4rem] '>
+                            <span className='text-16-500 text-gray-9'>주소</span>
+                            <span className='h-[7.2rem] w-[32rem] text-16-500 text-gray-9'>
+                              서울특별자치도 서울특별자치도 서울특별자치도 서울특별자치도 서울특별자치도 (000000) 105동
+                              103호
+                            </span>
+                          </div>
+                        </div>
+                      </AlertDialogDescription>
+                      <AlertDialogCancel>
+                        <Image
+                          src={'svgs/my-page-modal-close-icon.svg'}
+                          width={30}
+                          height={30}
+                          className='absolute h-[3rem] w-[3rem] flex-shrink-0'
+                          alt='모달 닫기 버튼 아이콘'
+                        />
+                      </AlertDialogCancel>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
-              ))}
-            </Fragment>
-          ))}
-        </div>
+                <div className='mt-[5rem] flex flex-col gap-[2.3rem]'>
+                  <div className='flex h-[3.9rem] w-[35.5rem] flex-row items-center justify-center gap-2'>
+                    <p className='flex-shrink-0 text-36-900'>{item.price + `원`}</p>
+                    <p className='text-28-500 text-gray-9'>{'낙찰'}</p>
+                  </div>
+                  <p className='ml-[3rem] h-[1.8rem] w-[27.1rem] flex-shrink-0 text-right text-18-700 text-gray-9'>
+                    {item.saleDate.slice(0, 10) + ` 경매 종료`}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Fragment>
+        ))}
       </div>
       {isFetchingNextPage ? <div>Loading...</div> : <div ref={ref}></div>}
     </>
