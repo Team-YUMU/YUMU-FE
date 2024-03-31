9db145f3bd491eb06cc7b2d6893cf09812957e18
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-03-31 17:17:20 +0900

    live 페이지 최신 UI 적용 (#169)
    * feat : BiddingBox 추가
    
    * feat : getAuctionDetail 적용
    
    * feat : streaming 페이지에도 적용
    
    * fix : detail 페이지와의 충돌 해결
    나중에 따로 수정해주세요.

--- src/pages/auction/[auctionid]/live/index.tsx
+++ src/pages/auction/[auctionid]/live/index.tsx
@@ -1,31 +1,60 @@
 import { BidHistories } from '@/components/domain/live/BidHistories';
-import { ExhibitionCarousel } from '@/components/common/ExhibitionCarousel';
 import { AuctionDetail } from '@/components/common/AuctionDetail';
 import { LiveVideo } from '@/components/domain/live/livevideo';
 import { LiveChatting } from '@/components/domain/live/livechatting';
-import { CircleAlert } from 'lucide-react';
 import ScrollButtons from '@/components/common/ScrollButtons';
+import InfoBox from '@/components/common/InfoBox';
+import BiddingBox from '@/components/domain/live/BiddingBox';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { GetAuctionDetailsProps } from '@/types/types';
+import { getAuctionDetails } from '@/services/api';
+import { ChevronLeft } from 'lucide-react';
 
 export default function Live() {
+  const [auctionDetailData, setAuctionDetailData] = useState<GetAuctionDetailsProps | null>();
+  const router = useRouter();
+  const { auctionid } = router.query;
+  const auctionId = Number(auctionid);
+
+  const getAuctionDetail = async () => {
+    try {
+      const data = await getAuctionDetails(auctionId);
+      console.log('경매 상세 정보:', data);
+      setAuctionDetailData(data);
+      // 필요한 처리 로직 추가
+    } catch (error) {
+      console.error('경매 상세 정보를 가져오는 동안 오류 발생:', error);
+    }
+  };
+
+  useEffect(() => {
+    getAuctionDetail();
+  }, [auctionid]);
+
   return (
     <div className='relative m-auto flex w-[138rem] select-none items-center gap-2 py-2'>
       <div className='grid grid-cols-3 gap-[1.25rem] px-2'>
-        <div className='col-span-3 bg-white font-TheJamsil text-36-400'>상품명(긴제목)</div>
+        <div className='col-span-3 flex flex-row items-center gap-[0.4rem] bg-white font-TheJamsil text-36-400'>
+          <button onClick={() => router.push('/search')}>
+            <ChevronLeft color='#e0e0e0' width={36} height={36} />
+          </button>
+          상품명(긴제목)
+        </div>
         <div className='col-span-2 flex w-[91rem] flex-col gap-[1.13rem]'>
           <div className='h-[54rem] w-full bg-stone-50'>
             비디오 켜지면 플리 이상하게 들려서 끄려고 대충 만든 녀석인데 비디오 컴포넌트 잘 만들어져 있으니까 걱정
             마세요. 사이즈도 동일합니다.
           </div>
+          <InfoBox className='mb-[2rem]' />
           {/* <LiveVideo /> */}
-          <div className='flex h-[8rem] w-full flex-row items-center space-x-[1.3rem] rounded-xl bg-stone-50 px-[3.2rem] text-16-700'>
-            <CircleAlert color='#bdbdbd' size={16} />
-            <p className='text-16-700 text-stone-300'>제주도 및 도서 산간 지역은 배송이 불가합니다.</p>
-          </div>
         </div>
         <LiveChatting />
-        <AuctionDetail />
-        <BidHistories />
-        <ExhibitionCarousel />
+        <AuctionDetail description={auctionDetailData?.artDescription} notice={auctionDetailData?.notice} />
+        <div className='space-y-[2rem]'>
+          <BiddingBox />
+          <BidHistories />
+        </div>
       </div>
       <ScrollButtons />
     </div>
--- src/pages/auction/[auctionid]/streaming/index.tsx
+++ src/pages/auction/[auctionid]/streaming/index.tsx
@@ -1,19 +1,44 @@
 import { BidHistories } from '@/components/domain/live/BidHistories';
-import { LiveChatting } from '@/components/domain/live/livechatting';
 import { AuctionDetail } from '@/components/common/AuctionDetail';
 import { LiveVideo } from '@/components/domain/live/livevideo';
-import { ChevronLeft, CircleAlert } from 'lucide-react';
+import { LiveChatting } from '@/components/domain/live/livechatting';
 import ScrollButtons from '@/components/common/ScrollButtons';
-import { Button } from '@/components/ui/button';
+import InfoBox from '@/components/common/InfoBox';
+import BiddingBox from '@/components/domain/live/BiddingBox';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { GetAuctionDetailsProps } from '@/types/types';
+import { getAuctionDetails } from '@/services/api';
+import { ChevronLeft } from 'lucide-react';
 
 export default function Streaming() {
+  const [auctionDetailData, setAuctionDetailData] = useState<GetAuctionDetailsProps | null>();
+  const router = useRouter();
+  const { auctionid } = router.query;
+  const auctionId = Number(auctionid);
+
+  const getAuctionDetail = async () => {
+    try {
+      const data = await getAuctionDetails(auctionId);
+      console.log('경매 상세 정보:', data);
+      setAuctionDetailData(data);
+      // 필요한 처리 로직 추가
+    } catch (error) {
+      console.error('경매 상세 정보를 가져오는 동안 오류 발생:', error);
+    }
+  };
+
+  useEffect(() => {
+    getAuctionDetail();
+  }, [auctionid]);
+
   return (
     <div className='relative m-auto flex w-[138rem] select-none items-center gap-2 py-2'>
       <div className='grid grid-cols-3 gap-[1.25rem] px-2'>
-        <div className='col-span-3 flex flex-row items-center gap-[1.2rem] bg-white font-TheJamsil text-36-400'>
-          <Button variant={'ghost'} className='p-0'>
-            <ChevronLeft color='#e0e0e0' className='size-[3.2rem] shrink-0' />
-          </Button>
+        <div className='col-span-3 flex flex-row items-center gap-[0.4rem] bg-white font-TheJamsil text-36-400'>
+          <button onClick={() => router.push('/search')}>
+            <ChevronLeft color='#e0e0e0' width={36} height={36} />
+          </button>
           상품명(긴제목)
         </div>
         <div className='col-span-2 flex w-[91rem] flex-col gap-[1.13rem]'>
@@ -21,15 +46,15 @@ export default function Streaming() {
             비디오 켜지면 플리 이상하게 들려서 끄려고 대충 만든 녀석인데 비디오 컴포넌트 잘 만들어져 있으니까 걱정
             마세요. 사이즈도 동일합니다.
           </div>
+          <InfoBox className='mb-[2rem]' />
           {/* <LiveVideo /> */}
-          <div className='flex h-[8rem] w-full flex-row items-center space-x-[1.3rem] rounded-xl bg-stone-50 px-[3.2rem] font-[notoKR] text-16-700'>
-            <CircleAlert color='#f9f9f9' fill='#bdbdbd' className='size-[1.8rem]' />
-            <p className='text-16-700 text-[#bdbdbd]'>제주도 및 도서 산간 지역은 배송이 불가합니다.</p>
-          </div>
         </div>
         <LiveChatting />
-        <AuctionDetail />
-        <BidHistories />
+        <AuctionDetail description={auctionDetailData?.artDescription} notice={auctionDetailData?.notice} />
+        <div className='space-y-[2rem]'>
+          <BiddingBox />
+          <BidHistories />
+        </div>
       </div>
       <ScrollButtons />
     </div>
