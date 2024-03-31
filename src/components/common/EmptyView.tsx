d2523a51af8cd9c49a400363b085b7eaaf6d18f0
김윤미 <younmeet@gmail.com>
2024-03-31 21:48:39 +0900

    refactor: EmptyView 컴포넌트 디자인 시안 적용

--- src/components/common/EmptyView.tsx
+++ src/components/common/EmptyView.tsx
@@ -1,11 +1,21 @@
+import Image from 'next/image';
+
 type EmptyViewProps = {
-  text: string;
+  title: string;
+  description: string;
+  className?: string;
 };
 
-export default function EmptyView({ text }: EmptyViewProps) {
+export default function EmptyView({ title, description, className }: EmptyViewProps) {
+  // const str = description.replace(/(?:\r\n|\r|\n)/g, '<br />');
+
   return (
-    <div className='flex items-center justify-center'>
-      <p className='text-16-400'>{text}</p>
+    <div
+      className={`flex flex-col items-center justify-center gap-[2rem] whitespace-pre-line py-[8rem] text-center text-gray-C5 ${className}`}
+    >
+      <Image src='/svgs/empty_view.svg' alt='' width={112} height={152} />
+      <h3 className='text-20-400'>{title}</h3>
+      <p className='text-16-500'>{description}</p>
     </div>
   );
 }
