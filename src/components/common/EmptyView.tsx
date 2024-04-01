10bf0256f64ba5147e0409e1316a0973eeb356c8
김윤미 <younmeet@gmail.com>
2024-04-02 01:27:28 +0900

    fix: text color 값 수정 및 주석 코드 삭제

--- src/components/common/EmptyView.tsx
+++ src/components/common/EmptyView.tsx
@@ -7,11 +7,9 @@ type EmptyViewProps = {
 };
 
 export default function EmptyView({ title, description, className }: EmptyViewProps) {
-  // const str = description.replace(/(?:\r\n|\r|\n)/g, '<br />');
-
   return (
     <div
-      className={`flex flex-col items-center justify-center gap-[2rem] whitespace-pre-line py-[8rem] text-center text-gray-C5 ${className}`}
+      className={`flex flex-col items-center justify-center gap-[2rem] whitespace-pre-line py-[8rem] text-center text-gray-C ${className}`}
     >
       <Image src='/svgs/empty_view.svg' alt='' width={112} height={152} />
       <h3 className='text-20-400'>{title}</h3>
