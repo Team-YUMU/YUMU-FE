31c4302b85c1c9d795e1042799a6d75030c2f662
박지원 <jackgg12322@gmail.com>
2024-04-04 00:46:59 +0900

    refactor : tanstack query data 활용

--- src/components/domain/myPage/Tabs/Edit/Edit.tsx
+++ src/components/domain/myPage/Tabs/Edit/Edit.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import React from 'react';
 import { Input } from '@/components/ui/input';
@@ -11,6 +10,7 @@ import UserDeleteModal from './UserDeleteModal';
 import { Separator } from '@/components/ui/separator';
 import { deleteMemberProfileImage, getMemberInfo, postAuthLogout, putMemberProfileImageData } from '@/services/api';
 import { useRouter } from 'next/router';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 interface FormData {
   email: string;
@@ -22,13 +22,13 @@ interface FormData {
 
 export default function Edit() {
   const { register, handleSubmit } = useForm<FormData>();
-  const [memberInfo, setMemberInfo] = useState<FormData>({
-    email: '',
-    nickname: '',
-    introduce: '',
-    profileImage: '',
-    loginStatus: '',
+
+  const { data: memberInfo } = useQuery<FormData>({
+    queryKey: ['memberData'],
+    queryFn: () => getMemberInfo(),
   });
+
+  const queryClient = useQueryClient();
 
   const router = useRouter();
 
@@ -42,29 +42,12 @@ export default function Edit() {
 
       try {
         await putMemberProfileImageData(formData);
-        memberInfoData();
+        queryClient.invalidateQueries({ queryKey: ['memberData'] });
       } catch (error) {
         console.error('이미지 업로드 오류:', error);
       }
     }
   };
-
-  useEffect(() => {
-    memberInfoData();
-  }, []);
-  const memberInfoData = async () => {
-    try {
-      const { ...res } = await getMemberInfo();
-      setMemberInfo(res);
-      console.log(res);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    memberInfoData();
-  }, []);
 
   const handleLogoutClick = async () => {
     try {
@@ -78,7 +61,7 @@ export default function Edit() {
   const handleImgDeleteClick = async () => {
     try {
       await deleteMemberProfileImage();
-      memberInfoData();
+      queryClient.invalidateQueries({ queryKey: ['memberData'] });
     } catch (error) {
       console.log(error);
     }
@@ -88,74 +71,78 @@ export default function Edit() {
     console.log(data);
   };
   return (
-    <div className='flex flex-col gap-[4rem]'>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='flex flex-col items-center justify-center gap-[2.1rem]'>
-          {memberInfo.profileImage ? (
-            <div className='border-1 relative h-[20rem] w-[20rem] rounded-[20rem]'>
-              <Image
-                src={memberInfo.profileImage}
-                alt='회원 이미지'
-                className=' h-full w-full rounded-[50rem] border-[0.1rem]'
-                fill
-              />
+    <>
+      {memberInfo ? (
+        <div className='flex flex-col gap-[4rem]'>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='flex flex-col items-center justify-center gap-[2.1rem]'>
+              {memberInfo?.profileImage ? (
+                <div className='border-1 relative h-[20rem] w-[20rem] rounded-[20rem]'>
+                  <Image
+                    src={memberInfo.profileImage}
+                    alt='회원 이미지'
+                    className=' h-full w-full rounded-[50rem] border-[0.1rem]'
+                    fill
+                  />
+                </div>
+              ) : (
+                <div className='border-1 relative h-[20rem] w-[20rem] rounded-[20rem]'>
+                  <Image
+                    src={memberInfo.profileImage}
+                    alt='회원 이미지'
+                    className=' h-full w-full rounded-[50rem] border-[0.1rem]'
+                    fill
+                  />
+                </div>
+              )}
+              <div className='flex flex-row items-center justify-center gap-2'>
+                <label
+                  htmlFor='picture'
+                  className='flex h-10 items-center  border-b-2 px-4 py-2 text-center text-14-400 leading-[2rem] text-gray-9'
+                >
+                  변경하기
+                </label>
+                <Input
+                  {...register('profileImage')}
+                  id='picture'
+                  type='file'
+                  className='hidden'
+                  accept='image/*'
+                  required
+                  multiple
+                  onChange={onChangeImg}
+                />
+                <Button
+                  type='button'
+                  variant={'myPageEditImageDelete'}
+                  size={'myPageEditImageDelete'}
+                  onClick={handleImgDeleteClick}
+                >
+                  <p className='text-center text-14-400 leading-[2rem] text-gray-9'>삭제</p>
+                </Button>
+              </div>
             </div>
-          ) : (
-            <div className='border-1 relative h-[20rem] w-[20rem] rounded-[20rem]'>
-              <Image
-                src={memberInfo.profileImage}
-                alt='회원 이미지'
-                className=' h-full w-full rounded-[50rem] border-[0.1rem]'
-                fill
-              />
+          </form>
+          <div className='inline-flex flex-col gap-[2rem]'>
+            <NickNameEditForm nickname={memberInfo.nickname} />
+            <IntroEditForm introduce={memberInfo.introduce} />
+            <div className='flex flex-col items-center gap-[1.2rem]'>
+              <div className='h-[2.3rem] w-[28rem] flex-shrink-0'>
+                <label className='text-12-500 leading-[2rem] text-gray-9'>아이디 정보</label>
+              </div>
+              <Separator orientation='vertical' className='h-[0.1rem] w-full bg-[#686868] p-0' />
+              <p className='h-[6rem] w-[28rem] flex-shrink-0 text-16-500 leading-[2rem] text-gray-9'>
+                {memberInfo.loginStatus === 'DEFAULT' ? memberInfo.email : '카카오로 로그인 되었습니다.'}
+              </p>
+              {memberInfo.loginStatus === 'DEFAULT' ? <NewPasswordModalForm /> : null}
+              <Button type='button' size='myPage' variant='myPage' onClick={handleLogoutClick}>
+                <span className='text-center text-16-500  text-gray-9'>로그아웃</span>
+              </Button>
+              <UserDeleteModal />
             </div>
-          )}
-          <div className='flex flex-row items-center justify-center gap-2'>
-            <label
-              htmlFor='picture'
-              className='flex h-10 items-center  border-b-2 px-4 py-2 text-center text-14-400 leading-[2rem] text-gray-9'
-            >
-              변경하기
-            </label>
-            <Input
-              {...register('profileImage')}
-              id='picture'
-              type='file'
-              className='hidden'
-              accept='image/*'
-              required
-              multiple
-              onChange={onChangeImg}
-            />
-            <Button
-              type='button'
-              variant={'myPageEditImageDelete'}
-              size={'myPageEditImageDelete'}
-              onClick={handleImgDeleteClick}
-            >
-              <p className='text-center text-14-400 leading-[2rem] text-gray-9'>삭제</p>
-            </Button>
           </div>
         </div>
-      </form>
-      <div className='inline-flex flex-col gap-[2rem]'>
-        <NickNameEditForm nickname={memberInfo.nickname} />
-        <IntroEditForm introduce={memberInfo.introduce} />
-        <div className='flex flex-col items-center gap-[1.2rem]'>
-          <div className='h-[2.3rem] w-[28rem] flex-shrink-0'>
-            <label className='text-12-500 leading-[2rem] text-gray-9'>아이디 정보</label>
-          </div>
-          <Separator orientation='vertical' className='h-[0.1rem] w-full bg-[#686868] p-0' />
-          <p className='h-[6rem] w-[28rem] flex-shrink-0 text-16-500 leading-[2rem] text-gray-9'>
-            {memberInfo.loginStatus === 'DEFAULT' ? memberInfo.email : '카카오로 로그인 되었습니다.'}
-          </p>
-          {memberInfo.loginStatus === 'DEFAULT' ? <NewPasswordModalForm /> : null}
-          <Button type='button' size='myPage' variant='myPage' onClick={handleLogoutClick}>
-            <span className='text-center text-16-500  text-gray-9'>로그아웃</span>
-          </Button>
-          <UserDeleteModal />
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 }
