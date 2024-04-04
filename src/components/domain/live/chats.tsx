771c0e6f23b0b6bb3a14a26635cdbb36dad91d4f
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-04-04 19:36:02 +0900

    feat : 자신의 닉네임과 프로필 이미지 적용 (#237)

--- src/components/domain/live/chats.tsx
+++ src/components/domain/live/chats.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react';
-import { Avatar, AvatarFallback } from '../../ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from '../../ui/avatar';
 
 interface ChatProps {
   memberId: string;
+  memberImage?: string;
+  mynickname?: string;
   children: ReactNode;
 }
 
-export function Chat({ memberId, children }: ChatProps) {
-  const checkIsUserMe = memberId === 'Me' ? 'flex-row-reverse text-end' : 'flex-row text-start';
+export function Chat({ memberId, memberImage, mynickname, children }: ChatProps) {
+  const checkIsUserMe = memberId === mynickname ? 'flex-row-reverse text-end' : 'flex-row text-start';
   return (
     <div className={`flex ${checkIsUserMe} items-center justify-start gap-[1.2rem]`}>
       <div className={`${checkIsUserMe} flex items-center justify-center gap-[0.8rem]`}>
         <Avatar className='h-[24px] w-[24px]'>
+          {/* <AvatarImage src={memberImage} /> */}
           <AvatarFallback className='border bg-white text-12-700'>{memberId[0]}</AvatarFallback>
         </Avatar>
         <p className='text-15-400 text-stone-300'>{memberId}</p>
