72c4c797e8cf326d976dfe32b5f20ec6aa61ab12
Sungeunji <danceunji@gmail.com>
2024-04-04 14:22:48 +0900

    feat: 로그인 페이지 사용자 정보 없음(404) 모달 작성

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -7,9 +7,19 @@ import { postAuthLogin } from '@/services/api';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import axios from 'axios';
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogAction,
+} from '@/components/ui/alert-dialog';
+import { useState } from 'react';
 
 export default function SignInPage() {
   const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   type FormData = {
     email: string;
@@ -24,18 +34,15 @@ export default function SignInPage() {
 
   const onSubmit = async (loginData: FormData) => {
     try {
-      const response = await postAuthLogin(loginData);
-      console.log(response);
-      router.push('/');
+      const responseApi = await postAuthLogin(loginData);
+      if (responseApi.response?.status === 200) {
+        router.push('/');
+      }
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.status === 404) {
-        console.log('가입되지 않은 사용자 입니다.');
-        console.log('axios 에러..!');
-      } else {
-        console.log('로그인 에러', error);
+        setIsModalOpen(true);
       }
     }
-    console.log('button clicked');
   };
 
   const KakaoLoginBaseURL = 'https://kauth.kakao.com/oauth/authorize';
@@ -59,64 +66,87 @@ export default function SignInPage() {
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-center'>
-      <div className='flex w-[43.8rem] flex-col items-center gap-[1.3rem] p-10'>
-        <div className=' mb-[2.6rem] flex flex-col items-center'>
-          <h1 className='font-TheJamsil text-[4.6rem] text-[#222] '>로그인</h1>
-          <h2 className='font-notoKR  text-[1.6rem] text-gray-9'>YUMU에 방문해주셔서 감사합니다.</h2>
-        </div>
-
-        <form
-          noValidate
-          className={`flex w-full flex-col items-center justify-center ${errors.email ? 'gap-[3.5rem]' : 'gap-[1rem]'}`}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <AuthInput
-            type='email'
-            required={!!errors.email}
-            placeholder='이메일을 입력해 주세요'
-            errorMessage={errors?.email?.message}
-            className=' h-[6.4rem] w-[43.8rem]'
-            {...register('email')}
-          />
-
-          <AuthInput
-            type='password'
-            required={!!errors.password}
-            errorMessage={errors?.password?.message}
-            className='h-[6.4rem] w-[43.8rem]'
-            {...register('password')}
-            placeholder='비밀번호를 입력해주세요'
-          />
-
-          <Button variant='default' className='mt-[2rem] bg-red-F text-[2rem]' type='submit' size='auth'>
-            로그인
-          </Button>
-        </form>
-
-        <div className=' flex flex-col gap-[1.2rem]'>
-          <div className='relative'>
-            <Image
-              src='/svgs/kakao-icon.svg'
-              height={25}
-              width={28}
-              alt='kakao'
-              className='absolute left-[2.8rem] mt-8'
-            />
-            <Button onClick={handleKakaoLogin} variant='sns' size='auth' className='bg-yellow text-[2rem] text-black-0'>
-              카카오로 로그인
-            </Button>
+      <AlertDialog>
+        <div className='flex w-[43.8rem] flex-col items-center gap-[1.3rem]'>
+          <div className=' mb-[2.6rem] flex flex-col items-center'>
+            <h1 className='font-TheJamsil text-[4.6rem] text-[#222] '>로그인</h1>
+            <h2 className='font-notoKR  text-[1.6rem] text-gray-9'>YUMU에 방문해주셔서 감사합니다.</h2>
           </div>
-          <Button
-            onClick={() => {
-              router.push('/signup');
-            }}
-            variant='outline'
-            size='auth'
-            className='border border-red-F bg-white text-[2rem] text-red-F'
+
+          <form
+            noValidate
+            className={`flex w-full flex-col items-center justify-center ${errors.email ? 'gap-[3.5rem]' : 'gap-[1rem]'}`}
+            onSubmit={handleSubmit(onSubmit)}
           >
-            회원가입
+            <AuthInput
+              type='email'
+              required={!!errors.email}
+              placeholder='이메일을 입력해 주세요'
+              errorMessage={errors?.email?.message}
+              className=' h-[6.4rem] w-[43.8rem]'
+              {...register('email')}
+            />
+
+            <AuthInput
+              type='password'
+              required={!!errors.password}
+              errorMessage={errors?.password?.message}
+              className='h-[6.4rem] w-[43.8rem]'
+              {...register('password')}
+              placeholder='비밀번호를 입력해주세요'
+            />
+
+            <AlertDialogTrigger asChild>
+              <Button variant='default' className='mb-5 mt-[2rem] bg-red-F text-[2rem]' type='submit' size='auth'>
+                로그인
+              </Button>
+            </AlertDialogTrigger>
+          </form>
+
+          {isModalOpen && (
+            <div className='flex flex-col items-center justify-center gap-8'>
+              <AlertDialogContent className='flex h-[17.9rem] flex-col items-center justify-around p-0'>
+                <div className='mt-[5rem] '>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle className='text-[1.6rem]'>가입되지 않은 사용자 입니다.</AlertDialogTitle>
+                  </AlertDialogHeader>
+                  <AlertDialogAction
+                    onClick={() => {
+                      router.push('/signin');
+                    }}
+                    className='rounded-[0.8rem mt-[4.1rem] h-[5rem] w-[32rem] border-t-2 bg-white text-[2rem] text-red-F hover:bg-white'
+                  >
+                    닫기
+                  </AlertDialogAction>
+                </div>
+              </AlertDialogContent>
+            </div>
+          )}
+        </div>
+      </AlertDialog>
+      <div className=' flex flex-col gap-[1.2rem]'>
+        <div className='relative'>
+          <Image
+            src='/svgs/kakao-icon.svg'
+            height={25}
+            width={28}
+            alt='kakao'
+            className='absolute left-[2.8rem] mt-8'
+          />
+          <Button onClick={handleKakaoLogin} variant='sns' size='auth' className=' bg-yellow text-[2rem] text-black-0'>
+            카카오로 로그인
           </Button>
         </div>
+        <Button
+          onClick={() => {
+            router.push('/signup');
+          }}
+          variant='outline'
+          size='auth'
+          className='border border-red-F bg-white text-[2rem] text-red-F'
+        >
+          회원가입
+        </Button>
       </div>
     </div>
   );
